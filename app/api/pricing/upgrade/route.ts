fc4afd339097a4b35a36e3e4fa7cc465a34e354a
Emmanuel SHYIRAMBERE <emashyirambere1@gmail.com>
2024-09-05 12:27:02 +0200

    Add handler to add or update subscription based on payment

--- app/api/pricing/upgrade/route.ts
+++ app/api/pricing/upgrade/route.ts
@@ -25,24 +25,19 @@ export async function POST(req: NextRequest) {
   }
 
   try {
-    // Handle the event
     switch (event.type) {
       case "charge.succeeded":
-        await handleSuccessfulCharge(event.data.object as Stripe.Charge);
+        await handleSuccessfulPayment(event.data.object as Stripe.Charge);
         break;
       case "payment_intent.succeeded":
-        await handleSuccessfulPaymentIntent(
-          event.data.object as Stripe.PaymentIntent
+        const paymentIntent = event.data.object as Stripe.PaymentIntent;
+        const charge = await stripe.charges.retrieve(
+          paymentIntent.latest_charge as string
         );
+        await handleSuccessfulPayment(charge);
         break;
-      case "payment_intent.created":
-        await handlePaymentIntentCreated(
-          event.data.object as Stripe.PaymentIntent
-        );
-        break;
-
       default:
-        console.log(`Unhandled event type ${event.type}`);
+        return;
     }
 
     return NextResponse.json({ received: true });
@@ -55,9 +50,9 @@ export async function POST(req: NextRequest) {
   }
 }
 
-async function handleSuccessfulCharge(charge: Stripe.Charge) {
+async function handleSuccessfulPayment(charge: Stripe.Charge) {
   const {
-    id,
+    id: chargeId,
     amount,
     customer,
     payment_method,
@@ -65,26 +60,127 @@ async function handleSuccessfulCharge(charge: Stripe.Charge) {
     billing_details,
   } = charge;
 
+  const user = await findOrCreateUser(
+    receipt_email,
+    billing_details?.email,
+    customer as string
+  );
+  if (!user) {
+    throw new Error("Unable to find or create user");
+  }
+
+  const pricingTier = await getPricingTier("Premium Plan");
+  if (!pricingTier) {
+    throw new Error("Pricing tier not found");
+  }
+
+  const subscription = await updateOrCreateSubscription(user, pricingTier);
+
+  if (!user.subscriptionId) {
+    await prisma.user.update({
+      where: { id: user.id },
+      data: { subscriptionId: subscription.id },
+    });
+  }
+
+  await createOrUpdateInvoice(
+    user.id,
+    subscription.id,
+    amount,
+    payment_method,
+    charge.payment_method_details,
+    chargeId
+  );
+}
+
+async function createOrUpdateInvoice(
+  userId: string,
+  subscriptionId: string,
+  amount: number,
+  paymentMethod: string | null,
+  paymentMethodDetails: Stripe.Charge.PaymentMethodDetails | null,
+  chargeId: string
+) {
+  const paymentMethodType = mapStripePaymentMethodToEnum(
+    paymentMethodDetails?.type
+  );
+  const last4 = (paymentMethodDetails?.card as any)?.last4 || "";
+
+  // Check if an invoice for this charge already exists
+  const existingInvoice = await prisma.invoice.findFirst({
+    where: {
+      userId,
+      subscriptionId,
+      amount: amount / 100,
+      paymentMethod: paymentMethod as string,
+      stripeChargeId: chargeId, // Add this field to your Invoice model
+    },
+  });
+
+  if (existingInvoice) {
+    // If the invoice exists, update it if necessary
+    return await prisma.invoice.update({
+      where: { id: existingInvoice.id },
+      data: {
+        status: "PAID",
+        paymentType: paymentMethodType,
+        lastFourCardDigits: last4,
+        updatedAt: new Date(),
+      },
+    });
+  } else {
+    // If the invoice doesn't exist, create a new one
+    return await prisma.invoice.create({
+      data: {
+        userId,
+        subscriptionId,
+        amount: amount / 100,
+        status: "PAID",
+        paymentMethod: paymentMethod as string,
+        paymentType: paymentMethodType,
+        lastFourCardDigits: last4,
+        paidAt: new Date(),
+        stripeChargeId: chargeId,
+      },
+    });
+  }
+}
+
+async function findOrCreateUser(
+  receiptEmail?: string | null,
+  billingEmail?: string | null,
+  customerId?: string
+) {
   const user = await prisma.user.findFirst({
     where: {
       OR: [
         {
           email: {
-            in: [receipt_email, billing_details?.email].filter(
-              Boolean
-            ) as string[],
+            in: [receiptEmail, billingEmail].filter(Boolean) as string[],
           },
         },
-        { subscriptionId: customer as string },
+        { subscriptionId: customerId },
       ].filter((condition) => Object.keys(condition).length > 0),
     },
   });
 
-  if (!user) {
-    throw new Error("You must be logged in");
-  }
+  if (user) return user;
+  throw new Error("User not found");
+}
 
-  // Find existing subscription or create a new one
+async function getPricingTier(tierName: string) {
+  return await prisma.pricingTier.findFirst({
+    where: { name: tierName },
+  });
+}
+
+async function updateOrCreateSubscription(user: any, pricingTier: any) {
+  const currentDate = new Date();
+  const subscriptionEndDate = getSubscriptionEndDate(
+    currentDate,
+    pricingTier.billingCycle
+  );
+
   let subscription = user.subscriptionId
     ? await prisma.subscription.findUnique({
         where: { id: user.subscriptionId },
@@ -92,54 +188,18 @@ async function handleSuccessfulCharge(charge: Stripe.Charge) {
       })
     : null;
 
-  let pricingTier;
-
-  if (subscription && subscription.pricingTier) {
-    pricingTier = subscription.pricingTier;
-  } else {
-    pricingTier = await prisma.pricingTier.findFirst({
-      where: { name: "Premium Plan" },
-    });
-
-    if (!pricingTier) {
-      console.error("Pricing tier not found");
-      return;
-    }
-  }
-
-  const currentDate = new Date();
-  let subscriptionEndDate: Date;
-
-  switch (pricingTier.billingCycle) {
-    case "MONTHLY":
-      subscriptionEndDate = new Date(
-        currentDate.setMonth(currentDate.getMonth() + 1)
-      );
-      break;
-    case "YEARLY":
-      subscriptionEndDate = new Date(
-        currentDate.setFullYear(currentDate.getFullYear() + 1)
-      );
-      break;
-    default:
-      console.error("Unknown billing cycle:", pricingTier.billingCycle);
-      return;
-  }
-
   if (subscription) {
-    // Update existing subscription
-    subscription = await prisma.subscription.update({
+    return await prisma.subscription.update({
       where: { id: subscription.id },
       data: {
         status: "ACTIVE",
         currentPeriodStart: currentDate,
         currentPeriodEnd: subscriptionEndDate,
       },
-      include: { pricingTier: true }, // Include pricingTier in the update result
+      include: { pricingTier: true },
     });
   } else {
-    // Create new subscription
-    subscription = await prisma.subscription.create({
+    return await prisma.subscription.create({
       data: {
         status: "ACTIVE",
         currentPeriodStart: currentDate,
@@ -154,60 +214,34 @@ async function handleSuccessfulCharge(charge: Stripe.Charge) {
       include: { pricingTier: true },
     });
   }
-
-  if (!user.subscriptionId) {
-    await prisma.user.update({
-      where: { id: user.id },
-      data: { subscriptionId: subscription.id },
-    });
-  }
-
-  if (payment_method) {
-    const paymentMethodDetails = charge.payment_method_details;
-    let paymentMethodType: PaymentMethodType = PaymentMethodType.CREDIT_CARD;
-
-    // Map Stripe payment method type to your PaymentMethodType enum
-    switch (paymentMethodDetails?.type) {
-      case "card":
-        paymentMethodType = PaymentMethodType.CREDIT_CARD;
-        break;
-      case "paypal":
-        paymentMethodType = PaymentMethodType.PAYPAL;
-        break;
-      case "bank_transfer":
-        paymentMethodType = PaymentMethodType.BANK_TRANSFER;
-        break;
-    }
-
-    const last4 = (paymentMethodDetails?.card as any)?.last4 || "";
-
-    // Create invoice
-    await prisma.invoice.create({
-      data: {
-        userId: user.id,
-        subscriptionId: subscription.id,
-        amount: amount / 100,
-        status: "PAID",
-        paymentMethod: payment_method as string,
-        paymentType: paymentMethodType,
-        lastFourCardDigits: last4,
-        paidAt: new Date(),
-      },
-    });
-  }
-
-  console.log("Successfully processed charge for user:", user.id);
 }
 
-async function handleSuccessfulPaymentIntent(
-  paymentIntent: Stripe.PaymentIntent
-) {
-  const charge = await stripe.charges.retrieve(
-    paymentIntent.latest_charge as string
-  );
-  await handleSuccessfulCharge(charge);
+function getSubscriptionEndDate(startDate: Date, billingCycle: string): Date {
+  const endDate = new Date(startDate);
+  switch (billingCycle) {
+    case "MONTHLY":
+      endDate.setMonth(endDate.getMonth() + 1);
+      break;
+    case "YEARLY":
+      endDate.setFullYear(endDate.getFullYear() + 1);
+      break;
+    default:
+      throw new Error(`Unknown billing cycle: ${billingCycle}`);
+  }
+  return endDate;
 }
 
-async function handlePaymentIntentCreated(paymentIntent: Stripe.PaymentIntent) {
-  console.log("Payment intent created:", paymentIntent.id);
+function mapStripePaymentMethodToEnum(
+  stripePaymentMethod: string | undefined
+): PaymentMethodType {
+  switch (stripePaymentMethod) {
+    case "card":
+      return PaymentMethodType.CREDIT_CARD;
+    case "paypal":
+      return PaymentMethodType.PAYPAL;
+    case "bank_transfer":
+      return PaymentMethodType.BANK_TRANSFER;
+    default:
+      return PaymentMethodType.CREDIT_CARD;
+  }
 }
