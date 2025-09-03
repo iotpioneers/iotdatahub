58c28603f108440ae4b534847cf7ca6dffbfff4d
SHYIRAMBERE Emmanuel <110242932+EmmanuelSHYIRAMBERE@users.noreply.github.com>
2025-09-03 11:25:31 +0200

    Add comprehensive TypeScript declarations

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,11 +1,3 @@
-declare module "express";
-declare module "cors";
-declare module "nodemailer";
-declare module "react-grid-layout";
-declare module "react-slick";
-declare module "react-grid-layout/css/styles.css";
-declare module "react-resizable/css/styles.css";
-
 import { IoTDataHubTCPServer } from "@/lib/iotdatahub-server";
 import { IoTDataHubWebSocketServer } from "@/lib/websocket-server";
 
@@ -13,3 +5,6 @@ declare global {
   var iotdatahubServer: IoTDataHubTCPServer | undefined;
   var wsServer: IoTDataHubWebSocketServer | undefined;
 }
+
+declare module "express";
+declare module "cors";
---
+++ types/react-grid-layout.d.ts
@@ -0,0 +1,3 @@
+declare module "react-grid-layout";
+declare module "react-grid-layout/css/styles.css";
+declare module "react-resizable/css/styles.css";
