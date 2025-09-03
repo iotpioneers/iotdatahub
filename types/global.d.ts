82b912e9c27ca451f2a64c326706271f329a74a3
SHYIRAMBERE Emmanuel <110242932+EmmanuelSHYIRAMBERE@users.noreply.github.com>
2025-09-03 18:11:43 +0200

    Fix the recurring TypeScript declaration file issues during Vercel deployment

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,7 +1,48 @@
-import { IoTDataHubTCPServer } from "@/lib/iotdatahub-server";
-import { IoTDataHubWebSocketServer } from "@/lib/websocket-server";
+import type { IoTDataHubTCPServer } from "@/lib/iotdatahub-server"
+import type { IoTDataHubWebSocketServer } from "@/lib/websocket-server"
 
 declare global {
-  var iotdatahubServer: IoTDataHubTCPServer | undefined;
-  var wsServer: IoTDataHubWebSocketServer | undefined;
+  var iotdatahubServer: IoTDataHubTCPServer | undefined
+  var wsServer: IoTDataHubWebSocketServer | undefined
+}
+
+declare module "ws" {
+  export * from "ws"
+}
+
+declare module "cors" {
+  export * from "cors"
+}
+
+declare module "bcrypt" {
+  export * from "bcrypt"
+}
+
+declare module "jsonwebtoken" {
+  export * from "jsonwebtoken"
+}
+
+declare module "nodemailer" {
+  export * from "nodemailer"
+}
+
+declare module "mongodb" {
+  export * from "mongodb"
+}
+
+declare module "body-parser" {
+  export * from "body-parser"
+}
+
+declare module "lodash" {
+  export * from "lodash"
+}
+
+declare module "uuid" {
+  export * from "uuid"
+}
+
+declare module "*" {
+  const content: any
+  export default content
 }
