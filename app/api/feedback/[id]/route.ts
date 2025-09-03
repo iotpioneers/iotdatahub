3da6691f5e5c060786294d669638c4810ac81def
SHYIRAMBERE Emmanuel <110242932+EmmanuelSHYIRAMBERE@users.noreply.github.com>
2025-09-03 20:26:29 +0200

    Fixed property 'isValid' does not exist on type 'typeof ObjectId

--- app/api/feedback/[id]/route.ts
+++ app/api/feedback/[id]/route.ts
@@ -1,16 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
-import { ObjectId } from "mongodb";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: { id: string } },
 ) {
-  // Check if the id is a valid ObjectId
-  if (!ObjectId.isValid(params.id)) {
-    return NextResponse.json({ error: "Invalid feedback ID" }, { status: 400 });
-  }
-
   const feedback = await prisma.feedback.findUnique({
     where: { id: params.id },
     include: { replies: true },
@@ -25,13 +19,8 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: { id: string } },
 ) {
-  // Check if the id is a valid ObjectId
-  if (!ObjectId.isValid(params.id)) {
-    return NextResponse.json({ error: "Invalid feedback ID" }, { status: 400 });
-  }
-
   const body = await request.json();
 
   const feedback = await prisma.feedback.findUnique({
@@ -58,13 +47,8 @@ export async function PUT(
 
 export async function DELETE(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: { id: string } },
 ) {
-  // Check if the id is a valid ObjectId
-  if (!ObjectId.isValid(params.id)) {
-    return NextResponse.json({ error: "Invalid feedback ID" }, { status: 400 });
-  }
-
   const feedback = await prisma.feedback.delete({
     where: { id: params.id },
   });
