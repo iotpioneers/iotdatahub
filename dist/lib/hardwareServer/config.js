c05f9d7e312d0e23414e07bab1dc4c1b5f30a144
SHYIRAMBERE Emmanuel <110242932+EmmanuelSHYIRAMBERE@users.noreply.github.com>
2025-09-21 15:24:15 +0200

    Build project for deployment compatibility

--- dist/lib/hardwareServer/config.js
+++ dist/lib/hardwareServer/config.js
@@ -1,7 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const config = {
-    iotPort: parseInt(process.env.IOT_PORT || process.env.IOT_PORT_ALTERNATIVE || "8080", 10),
+    iotPort: parseInt(process.env.PORT ||
+        process.env.IOT_PORT ||
+        process.env.IOT_PORT_ALTERNATIVE ||
+        "7000", 10),
     iotSSLPort: parseInt(process.env.IOT_SSL_PORT || "8443", 10),
     logLevel: process.env.LOG_LEVEL || "INFO",
     apiBaseUrl: process.env.API_BASE_URL || process.env.NEXT_PUBLIC_BASE_URL,
