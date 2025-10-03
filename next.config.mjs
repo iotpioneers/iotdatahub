e75f1b5c06136f7fef3b6e8688d7500fc8e150d1
SHYIRAMBERE Emmanuel <110242932+EmmanuelSHYIRAMBERE@users.noreply.github.com>
2025-10-03 14:15:43 +0200

    Fix failed to load next.config.js

---
+++ next.config.mjs
@@ -0,0 +1,30 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:paths*",
+        headers: [
+          {
+            key: "referrer-policy",
+            value: "no-referrer",
+          },
+        ],
+      },
+    ];
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": __dirname,
+    };
+    config.externals = config.externals
+      ? [...config.externals, "bcrypt"]
+      : ["bcrypt"];
+    return config;
+  },
+};
+
+export default nextConfig;
